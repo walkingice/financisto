e505cd31e42cabe165a2b4e8479586e70262c237
Julian_Chu <walkingice0204@gmail.com>
2020-08-10 00:39:05 +0900

    remove search filter from Templates list

--- app/src/main/java/ru/orangesoftware/financisto2/template/SelectTemplateFragment.kt
+++ app/src/main/java/ru/orangesoftware/financisto2/template/SelectTemplateFragment.kt
@@ -4,26 +4,16 @@ import android.app.Activity
 import android.content.Intent
 import android.database.Cursor
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ListAdapter
-import android.widget.TextView
 import ru.orangesoftware.financisto.R
-import ru.orangesoftware.financisto.activity.MyEntityListActivity
 import ru.orangesoftware.financisto.adapter.TemplateListAdapter
-import ru.orangesoftware.financisto.blotter.BlotterFilter
-import ru.orangesoftware.financisto.filter.Criteria
-import ru.orangesoftware.financisto.filter.WhereFilter
-import ru.orangesoftware.financisto.widget.SearchFilterTextWatcherListener
 
 class SelectTemplateFragment : TemplatesListFragment() {
 
     override fun getLayoutResourceId(): Int = R.layout.fragment_select_template
-    private var multiplierText: TextView? = null
-    private var searchFilter: EditText? = null
 
     override fun internalOnCreate(view: View, savedInstanceState: Bundle?) {
         internalOnCreateTemplates(view)
@@ -44,37 +34,6 @@ class SelectTemplateFragment : TemplatesListFragment() {
             requireActivity().setResult(Activity.RESULT_CANCELED)
             requireActivity().finish()
         }
-        multiplierText = view.findViewById(R.id.multiplier)
-
-        searchFilter = view.findViewById(R.id.searchFilter)
-        searchFilter!!.addTextChangedListener(object :
-            SearchFilterTextWatcherListener(MyEntityListActivity.FILTER_DELAY_MILLIS) {
-            override fun clearFilter(oldFilter: String) {
-                blotterFilter.remove(BlotterFilter.TEMPLATE_NAME)
-            }
-
-            override fun applyFilter(filter: String) {
-                var filter = filter
-                if (!TextUtils.isEmpty(filter)) {
-                    filter = "%" + filter.replace(" ", "%") + "%"
-                    blotterFilter.put(
-                        Criteria.or(
-                            Criteria(
-                                BlotterFilter.TEMPLATE_NAME,
-                                WhereFilter.Operation.LIKE,
-                                filter
-                            ),
-                            Criteria(
-                                BlotterFilter.CATEGORY_NAME,
-                                WhereFilter.Operation.LIKE,
-                                filter
-                            )
-                        )
-                    )
-                }
-                listViewController.recreateCursor()
-            }
-        })
     }
 
     override fun createCursor(): Cursor {
